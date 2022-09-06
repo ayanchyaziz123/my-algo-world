3b728ac93b55c69ff2addbce869aeefbc2b0a01d
azizchyayan123 <aaziz9642@gmail.com>
2022-09-06 22:21:12 +0600

    1st commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import { Container } from '@mui/system';
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <Container style={{backgroundColor: '#fff'}}>
+    <ResponsiveAppBar/>
+    <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
